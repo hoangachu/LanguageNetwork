227f59fdd2f2ed0b042339e2ecd91dace7afb831
hoàng huy <huyhoang121296@gmail.com>
2021-04-27 09:16:15 +0700

    update

--- LanguageNetwork/RoundTheCode.GoogleAuthentication/wwwroot/js/Ask.js
+++ LanguageNetwork/RoundTheCode.GoogleAuthentication/wwwroot/js/Ask.js
@@ -3,8 +3,8 @@
 })();
 // sau khi load xong page
 jQuery(window).load(function () {
-    $('.sidebarleft .sidebar').css('display', 'none');
-    $('.sidebarleft').css('background-image', '/images/backgroundsidebar_ask.png');
-    $('.sidebarright .widget').css('display', 'none');
-    $('.btn-load-more').css('display', 'none');
+    //$('.sidebarleft .sidebar').css('display', 'none');
+    //$('.sidebarleft').css('background-image', '/images/backgroundsidebar_ask.png');
+    //$('.sidebarright .widget').css('display', 'none');
+    //$('.btn-load-more').css('display', 'none');
 });
