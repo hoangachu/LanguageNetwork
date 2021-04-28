991dce5d7d4d9cf3637712d6a066ea2d3e2e8a80
hoàng huy <huyhoang121296@gmail.com>
2021-04-29 01:03:38 +0700

    update

--- LanguageNetwork/RoundTheCode.GoogleAuthentication/wwwroot/js/Ask.js
+++ LanguageNetwork/RoundTheCode.GoogleAuthentication/wwwroot/js/Ask.js
@@ -7,11 +7,11 @@
         else if ($('.bootstrap-tagsinput').find('span').length > 10) {
             $('.bootstrap-tagsinput').find('span').last().remove();
             $('.bootstrap-tagsinput').find('span.tag.label.label-info').last().remove();
-            ErrorMessage('error');
+            ErrorMessage('Do not enter more than 5 tags');
         }
         else {
             
-            ErrorMessage('error');
+           /* ErrorMessage('error');*/
         }
         $('.bootstrap-tagsinput').find('input').attr('placeholder', '');
     });
